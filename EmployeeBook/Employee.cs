1dfa8e44d3b36ee6842d4281bbe95ff9a689f25a
Anuradha Vadde <vaddeanuradha@gmail.com>
2019-02-14 13:39:18 -0800

    Initial Commit

---
+++ EmployeeBook/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeBookApp
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public static Employee GetEmployee()
+        {
+            var employee = new Employee
+            {
+                Name = "Jhon Doe",
+                Title="CEO"
+            };
+            return employee;
+            
+        }
+        public static void WriteEmployee(Employee employee)
+        {
+            var employeeData = $"{employee.Name},{employee.Title}";
+            FileHelper.WriteTextFileAsync("EmployeeTextFile", employeeData);
+        }
+    }
+}
---
+++ EmployeeBook/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+using Windows.Storage.Streams;
+
+namespace EmployeeBookApp
+{
+    public static class FileHelper
+    {
+        public static async void WriteTextFileAsync
+            (string filename,string content)
+        {
+           
+            var localFolder = ApplicationData.Current.LocalFolder;
+            var textFile = await localFolder.CreateFileAsync
+                (filename, CreationCollisionOption.OpenIfExists);
+            var textStream = await textFile.OpenAsync(FileAccessMode.ReadWrite);
+            var textWriter = new DataWriter(textStream);
+            textWriter.WriteString(content);
+            await textWriter.StoreAsync();
+
+        }
+    }
+}
